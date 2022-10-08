5ad295884d798531b3a8300fd604188011818b64
shaktimanrai <98007119+shaktimanrai@users.noreply.github.com>
2022-10-08 03:32:58 -0700

    Calculatter

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { add, sub, mult, div } from './components/Calculatter';
+
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>sum of two no is {add(40, 4)}</li>
+        <li>sub of two no is {sub(40, 4)}</li>
+        <li>mult of two no is {mult(40, 4)}</li>
+        <li>div of two no is {div(40, 4)}</li>
+      </ul>
+    </>
+
+  );
+};
+
+export default App;
---
+++ src/components/Calculatter.js
@@ -0,0 +1,29 @@
+
+
+
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+
+function sub(a, b) {
+    let sub = a - b;
+    return sub;
+}
+
+
+function mult(a, b) {
+    let mult = a * b;
+    return mult;
+}
+
+function div(a, b) {
+    let div = a / b;
+
+    //add the point  value
+    div= div.toFixed(2);
+    return div;
+}
+
+export { add, sub, mult, div }
